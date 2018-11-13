df8c0c39eb1ac6c02761aa417a55e8e05959a95c
Juan Beresiarte <35072260+kali-linux-sh@users.noreply.github.com>
2018-11-13 17:24:08 -0300

    Update alzar.py

--- hack-pack/alzar.py
+++ hack-pack/alzar.py
@@ -4,10 +4,10 @@ from random import choice
 
 try:
 	valores = str(argv[0])
-    longitud = int(argv[1])
+	longitud = int(argv[1])
 except Exception as e:
-    print("- Termino 1: Valores")
-    print("- Termino 2: Longitud")
+	print("- Termino 1: Valores")
+	print("- Termino 2: Longitud")
 
 p = ""
 p = p.join([choice(valores) for i in range(longitud)])
